d252b68076c203d86229ce7a78cad6462641d031
Lorena377 <alamo_roman@hotmail.com>
2019-03-19 08:40:09 -0500

    mostrando el nombre de un pokemon en la consola

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 /* Manejo del DOM */
+const mostrar =POKEMON.pokemon;
+for (let index = 0; index < mostrar.length; index++) {
+    const element = mostrar[index];
+    }
+
+console.log(mostrar[0].name);
+    
+//console.log(element.name);
