be8e49b69953f8fece5a67607ba5a43ed14ba5fb
melilove <melissa.salazar13@yahoo.com>
2019-03-19 08:46:18 -0500

    Imprimiendo nombre,imagen y tipo de la data

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 const mostrarPokemones = (data) => {
   let arrayMostrar = [];
   for (let i = 0; i < data.length; i++) {
-    arrayMostrar.push({name: data[i].name, img: data[i].img, type: data[i].type});
+    arrayMostrar.push({name: data[i].name, img: data[i].img, type: data[i].type, weaknesses: data[i].weaknesses, id:data[i]});
   }
   return arrayMostrar;
 };
