71a5f0057657354f62b6f541e54b94326d48e546
Lorena377 <alamo_roman@hotmail.com>
2019-03-21 09:46:53 -0500

    mostrando los nombres de los pokemon y ordenandolos de A-Z

--- src/data.js
+++ src/data.js
@@ -8,5 +8,41 @@
 // };
 
 // window.example = example;
+const arrayPokemon = POKEMON.pokemon
 
-const datosPokemon = POKEMON.pokemon;
+//imprimiento data
+//console.log(arrayPokemon);
+
+//consoleando el nombre de un pokemon
+//console.log(arrayPokemon[1].name)
+
+// consolenado los nombres de todos los pokemones
+/*for ( a=0; a<arrayPokemon.length; a ++){
+    console.log(arrayPokemon[a].name);
+}*/
+
+//ordenando de A-Z
+
+
+const newarray = [];
+const arraylongitud = POKEMON.pokemon;
+
+for ( let x=0; x<arraylongitud.length; x++){
+    newarray.push(arraylongitud[x].name);
+}
+ console.log(newarray.sort().reverse(newarray));
+ 
+
+
+
+
+//return(arrayPokemon.name[1]);
+
+
+
+
+/*
+for ( let x =0; x<arrlongitud.length; x++){
+    newarray.push(arrlongitud[x].name);
+   }
+   console.log(newarray.sort());*/
--- src/main.js
+++
@@ -1,9 +0,0 @@
-/* Manejo del DOM */
-const mostrar =POKEMON.pokemon;
-for (let index = 0; index < mostrar.length; index++) {
-    const element = mostrar[index];
-    }
-
-console.log(mostrar[0].name);
-    
-//console.log(element.name);
