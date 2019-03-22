a33b642660764ce114233eb274ee4243da1d15fd
melilove <melissa.salazar13@yahoo.com>
2019-03-22 17:39:54 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -1,21 +1,14 @@
-const arrayPokemon = POKEMON.pokemon;
-// console.log(arrayPokemon);
+const dataPokemon = POKEMON.pokemon;
 const containerList = document.getElementById('container-list');
-const arrayMostrado = pokemon.mostrarPokemones(arrayPokemon);
-
-const crearPlantilla = (data) => {
-    let plantilla = '';
-    data.forEach((data) => {
-      let carta = `
-      <div class = "card-link">
-      <label class="name">${ data.name }</label>
-      <img class="image" src="${ data.img }" /> 
-      <label class="type">${ data.type }</label>
-      <label class="weaknesses">${ data.weaknesses }</label>
-      </div>
-      `;
-      plantilla += carta;
-    });
-    containerList.innerHTML = plantilla;
-  };
-  crearPlantilla(arrayMostrado);
+  for (let i = 0; i < dataPokemon.length; i++) {
+    containerList.innerHTML += `
+    <div class = "card" style="width: 12rem;">
+    <img class="image" src="${ dataPokemon[i].img }" class="card-img-top" />
+    <div class="card-body card-header"> 
+    <label class="card-text"> NOMBRE : ${ dataPokemon[i].name }</label>
+    <label class="card-text"> Tipo : ${ dataPokemon[i].type }</label>
+    <label class="card-text"> Debilidad : ${ dataPokemon[i].weaknesses }</label>
+    </div>
+    </div> 
+    `;
+  }
