52bf373af832da3e4fbd3c9c1852d5e7f125a5ed
Lorena377 <alamo_roman@hotmail.com>
2019-03-22 17:02:33 -0500

    pintando el nombre, imagen y tipo de los pokemones en la web

--- src/data.js
+++ src/data.js
@@ -1,10 +1,4 @@
-const mostrarPokemones= (data) =>{
- let newarray= [];
-for (let i = 0; i < data.length; i++) {
- newarray.push({name: data[i].name, type: data[i].type, img: data[i].img, weaknesses: data[i].weaknesses});
-}
-return newarray;
-};
+
 
 window.pokemon ={
   mostrarPokemones
--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 const dataPokemon = POKEMON.pokemon;
-const container = document.getElementById("container-list");
-const mostrarPokemon = pokemon.mostrarPokemones(dataPokemon);
+const containerList= document.getElementById("container-list");
 
-const newPlantilla = (plantilla)=>{
-    let crear ='';
-    plantilla.forEach((plantilla) =>{
-            })
-        
-    });
+for(let i=0; i<dataPokemon.length; i++){
+    containerList.innerHTML+=`
+    <div class="card-link">
+    <label class="name">${ dataPokemon[i].name }</label>
+    <img class="image" src="${ dataPokemon[i].img }" />
+    <label class="name">${ dataPokemon[i].type}</label>
+    </div>
+`;
 }
+
+
+
+//con
+//const container = document.getElementById("container-list");
+//const mostrarPokemon = pokemon.mostrarPokemones(dataPokemon);
+
+//mostrando el nombre de los pokemones
+/*for (a=0; a<dataPokemon.length; a++){
+    console.log(dataPokemon[a].name);
+}*/
