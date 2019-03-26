33f735b4647cca72b943a9d7ad6404d6b68d9538
melilove <melissa.salazar13@yahoo.com>
2019-03-26 10:02:42 -0500

    bajando cambios de lore

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 const mostrarPokemones=(data)=>{
     let arrayMostrar =[];
     for (let i = 0; i < data.length; i++) {
-        arrayMostrar.push({name:data[i].name, img:data[i].img, })
-        
+        arrayMostrar.push({name:data[i].name, img:data[i].img, type:data[i].type, weaknesses:data[i].weaknesses})  
     }
     return arrayMostrar;
-}
+};
+
 
 window.pokemon={
     mostrarPokemones
