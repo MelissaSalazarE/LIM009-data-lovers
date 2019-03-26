224f979987264530951e411a76a0b268f57c9bb1
Lorena377 <alamo_roman@hotmail.com>
2019-03-26 09:37:36 -0500

    ordenando de AZ y de ZA en consola

--- src/data.js
+++ src/data.js
@@ -1,5 +1,34 @@
+const arrayNombres =POKEMON.pokemon;
+newarray = [];
+for(i=0; i<arrayNombres.length; i++){
+	newarray.push(arrayNombres[i].name);
+	}
 
+function ordenarAZ(a, b) {
+	if(typeof a === 'string' && typeof b === 'string'){
+	  a = a.toLowerCase();
+	  b = b.toLowerCase();  
+	}  
+	if (a > b) {
+	  return 1;
+	} else if (a < b) {
+	  return -1;
+	} else if (a === b) {
+	  return 0;
+	}
+  }
+  //console.log(newarray.sort(ordenar));
 
-window.pokemon ={
-  mostrarPokemones
+  function ordenarZA(a, b) {
+	a = a.toLowerCase();
+	b = b.toLowerCase(); 
+
+  if (a > b) {
+	return 1;
+  } else if (a < b) {
+	return -1;
+  } else if (a === b) {
+	return 0;
+  }
 }
+console.log(newarray.sort(ordenarZA).reverse());
