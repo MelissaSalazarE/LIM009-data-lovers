cec96aaf07a6fabe19215ff71ac25ed8e31c5235
melilove <melissa.salazar13@yahoo.com>
2019-04-02 10:29:55 -0500

    corigiendo errores por espacios n html

--- src/data.js
+++ src/data.js
@@ -1,81 +1,71 @@
 const crearListaDePokemones = (data) => {
-	let arrayMostrar = [];
-	for (let i = 0; i < data.length; i++) {
-		arrayMostrar.push({ name: data[i].name, img: data[i].img, type: data[i].type, weaknesses: data[i].weaknesses })
-	}
-	return arrayMostrar;
+  let arrayMostrar = [];
+  for (let i = 0; i < data.length; i++) {
+    arrayMostrar.push({ name: data[i].name, img: data[i].img, type: data[i].type, weaknesses: data[i].weaknesses });
+  }
+  return arrayMostrar;
 };
 
 const ordenarPokemones = (data, tipo) => {
-	let ordenar = [];
-	for (let i = 0; i < data.length; i++) {
-		ordenar.push({ name: data[i].name, img: data[i].img, type: data[i].type, weaknesses: data[i].weaknesses });
-	}
-	ordenar.sort((firtsName, lastName) => {
-		if (firtsName.name > lastName.name) {
-			return 1;
-		}
-		if (firtsName.name < lastName.name) {
-			return -1;
-		}
-		return 0;
-	});
+  let ordenar = [];
+  for (let i = 0; i < data.length; i++) {
+    ordenar.push({ name: data[i].name, img: data[i].img, type: data[i].type, weaknesses: data[i].weaknesses });
+  }
+  ordenar.sort((firtsName, lastName) => {
+    if (firtsName.name > lastName.name) {
+      return 1;
+    }
+    if (firtsName.name < lastName.name) {
+      return -1;
+    }
+    return 0;
+  });
 
-	if (tipo === 'Az') {
-		return ordenar;
-	}
-	if (tipo === 'Za') {
-		return ordenar.reverse();
-	}
-
+  if (tipo === 'Az') {		
+    return ordenar;	
+  }
+  if (tipo === 'Za') {
+    return ordenar.reverse();
+  }
 };
 
 const filtrarPokemones = (data, tipo) => {
-	let arrayFiltrar = [];
-	arrayFiltrar = data
-		.filter((contenido) => {
-			for (let i = 0; i < contenido.type.length; i++) {
-				if (contenido.type[i] === tipo) {
-					return 1;
-
-				}
-			}
-		});
-	return arrayFiltrar;
-}
-
+  let arrayFiltrar = [];
+  arrayFiltrar = data
+    .filter((contenido) => {
+      for (let i = 0; i < contenido.type.length; i++) {
+        if (contenido.type[i] === tipo) {
+          return 1;
+        }
+      }
+    });
+  return arrayFiltrar;
+};
 
 const filtrarPorDebilidad = (data, tipo) => {
-	let arrayDebilidad = [];
-	arrayDebilidad = data
-		.filter((contenido) => {
-
-			for (let i = 0; i < contenido.weaknesses.length; i++) {
-				if (contenido.weaknesses[i] === tipo) {
-					return 1;
-
-				}
-			}
-		});
-
-
-	return arrayDebilidad;
-
-}
+  let arrayDebilidad = [];
+  arrayDebilidad = data
+    .filter((contenido) => {
+      for (let i = 0; i < contenido.weaknesses.length; i++) {
+        if (contenido.weaknesses[i] === tipo) {
+          return 1;
+        }
+      }
+    });
+  return arrayDebilidad;
+};
 
 const estadistica = (array, data) => {
-	let resultado = array.length / data.length;
-	result = resultado.toFixed(2);
-	return result;
-
-}
-
+  let resultado = array.length / data.length;
+  let result = resultado.toFixed(2);
+  return result;
+};
 
 
 window.pokemon = {
-	crearListaDePokemones,
-	ordenarPokemones,
-	filtrarPokemones,
-	filtrarPorDebilidad,
-	estadistica,
-}
+  crearListaDePokemones,
+  ordenarPokemones,
+  filtrarPokemones,
+  filtrarPorDebilidad,
+  estadistica,
+};
--- src/main.js
+++ src/main.js
@@ -8,29 +8,29 @@ const paginapokedex = document.getElementById('segundaPantalla');
 const paginaEstadistica = document.getElementById('terceraPantalla');
 const container = document.getElementById('container-list');
 
-paginaInicio.style.display = "block";
-paginapokedex.style.display = "none";
-paginaEstadistica.style.display = "none";
-container.style.display = "none";
+paginaInicio.style.display = 'block';
+paginapokedex.style.display = 'none';
+paginaEstadistica.style.display = 'none';
+container.style.display = 'none';
 
 seccionInicio.addEventListener('click', () => {
-	paginaInicio.style.display = "block";
-	paginapokedex.style.display = "none";
-	paginaEstadistica.style.display = "none";
+  paginaInicio.style.display = 'block';
+  paginapokedex.style.display = 'none';
+  paginaEstadistica.style.display = 'none';
 });
 
 seccionPokedex.addEventListener('click', () => {
-	paginaInicio.style.display = "none";
-	paginapokedex.style.display = "block";
-	paginaEstadistica.style.display = "none";
-	container.style.display = "block";
+  paginaInicio.style.display = 'none';
+  paginapokedex.style.display = 'block';
+  paginaEstadistica.style.display = 'none';
+  container.style.display = 'block';
 });
 
 seccioEstadistica.addEventListener('click', () => {
-	paginaInicio.style.display = "none";
-	paginapokedex.style.display = "none";
-	paginaEstadistica.style.display = "block";
-	container.style.display = "block";
+  paginaInicio.style.display = 'none';
+  paginapokedex.style.display = 'none';
+  paginaEstadistica.style.display = 'block';
+  container.style.display = 'block';
 });
 
 // creando las funciones de la primera historia //
@@ -40,9 +40,9 @@ const pokemonesOrdenados = document.getElementById('orden-pokemon');
 const dataPokemon = POKEMON.pokemon;
 const arrayPokemon = pokemon.crearListaDePokemones(dataPokemon);
 const listaDePokemones = (data) => {
-	let mostrar = '';
-	for (let i = 0; i < data.length; i++) {
-		cartilla = `
+  let mostrar = '';
+  for (let i = 0; i < data.length; i++) {
+    let cartilla = `
     <div class="card-link">
     <img class="list-group-item" src="${ data[i].img}" />
     <p class="ist-group-item"> Nombre : ${ data[i].name}</p>
@@ -50,33 +50,33 @@ const listaDePokemones = (data) => {
     <p class="ist-group-item"> Debilidad : ${ data[i].weaknesses}</p>
     </div>
 `;
-		mostrar += cartilla;
-	};
-	containerList.innerHTML = mostrar;
-}
+    mostrar += cartilla;
+  }
+  containerList.innerHTML = mostrar;
+};
 listaDePokemones(arrayPokemon);
 // la funcion para el ordenado //
-pokemonesOrdenados.addEventListener("change", () => {
-	const ordenando = pokemon.ordenarPokemones(dataPokemon, pokemonesOrdenados.value);
-	listaDePokemones(ordenando);
+pokemonesOrdenados.addEventListener('change', () => {
+  const ordenando = pokemon.ordenarPokemones(dataPokemon, pokemonesOrdenados.value);
+  listaDePokemones(ordenando);
 });
 // filtrado //
 
-const pokemonesFiltrados = document.getElementById("tipo-pokemon");
-pokemonesFiltrados.addEventListener("change", () => {
-	const filtrando = pokemon.filtrarPokemones(dataPokemon, pokemonesFiltrados.value);
-	listaDePokemones(filtrando);
+const pokemonesFiltrados = document.getElementById('tipo-pokemon');
+pokemonesFiltrados.addEventListener('change', () => {
+  const filtrando = pokemon.filtrarPokemones(dataPokemon, pokemonesFiltrados.value);
+  listaDePokemones(filtrando);
 });
 
-//filtrar por debilidad y mostrando estadstica //
+//  filtrar por debilidad y mostrando estadstica  //
 
-const porcentaje = document.getElementById("porcentaje");
-const pokemonesPorDebilidad = document.getElementById("debilidad-pokemon");
+const porcentaje = document.getElementById('porcentaje');
+const pokemonesPorDebilidad = document.getElementById('debilidad-pokemon');
 
-pokemonesPorDebilidad.addEventListener("change", () => {
-	const filtrandoPorDebilidad = pokemon.filtrarPorDebilidad(dataPokemon, pokemonesPorDebilidad.value);
-	listaDePokemones(filtrandoPorDebilidad);
-	const estadis = pokemon.estadistica(filtrandoPorDebilidad, dataPokemon)
-	porcentaje.innerHTML = estadis;
+pokemonesPorDebilidad.addEventListener('change', () => {
+  const filtrandoPorDebilidad = pokemon.filtrarPorDebilidad(dataPokemon, pokemonesPorDebilidad.value);
+  listaDePokemones(filtrandoPorDebilidad);
+  const estadis = pokemon.estadistica(filtrandoPorDebilidad, dataPokemon);
+  porcentaje.innerHTML = estadis;
 });
 
